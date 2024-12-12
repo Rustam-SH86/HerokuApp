bd2bfc7d7497213bbd2549ce58e54dda3e729bfd
rustam20.04 <rust-shafik@yandex.ru>
2024-12-12 22:43:34 +0400

    new_last

--- src/test/java/HerokuAppTests/СheckAddRemoveElementTest.java
+++ src/test/java/HerokuAppTests/СheckAddRemoveElementTest.java
@@ -1,6 +1,9 @@
 package HerokuAppTests;
 
 import base.SetUp;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -8,8 +11,10 @@ import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
-
-public class checkAddRemoveElementTest extends SetUp {
+@Log4j2
+public class СheckAddRemoveElementTest extends SetUp {
+    @Step("Проверяем удаление и добавление элемента")
+    @Description("удаление и добавление элемента")
     @Test
     public void checkAddRemoveElement() {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
