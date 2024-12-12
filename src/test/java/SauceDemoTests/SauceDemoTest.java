6e567b11461d97dab7b345f2d603fdc378b90359
rustam20.04 <rust-shafik@yandex.ru>
2024-12-12 17:47:19 +0400

    доавил степ на авторизацию

--- src/test/java/SauceDemoTests/SauceDemoTest.java
+++ src/test/java/SauceDemoTests/SauceDemoTest.java
@@ -1,10 +1,12 @@
 package SauceDemoTests;
 
 import base.SetUp;
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import pages.SauceDemoPage;
 
 public class SauceDemoTest extends SetUp {
+    @Step("авторизация на сайте")
     @Test
     public void loginToSauceDemo() {
         SauceDemoPage sauceDemoPage = new SauceDemoPage(driver);
